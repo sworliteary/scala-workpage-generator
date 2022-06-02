3a3ee38e1115c456be819acf58a85f24c41cb603
sworltieary <shnovels@gmail.com>
2022-06-03 00:32:56 +0900

    とりあえずglobもどきを試す

--- src/main/scala/workbuilder/Main.scala
+++ src/main/scala/workbuilder/Main.scala
@@ -1,12 +1,25 @@
 package workbuilder
 
+import java.io.File
+import collection.JavaConverters._
+
 object Main {
-  def main(args: Array[String]) {
+  def listFilesRecursive(dir: File, fileName: String):  Array[File] = {
+    val these = dir.listFiles.filter(_.getPath.endsWith(fileName))
+    these ++ dir.listFiles.filter(_.isDirectory).flatMap(d => listFilesRecursive(d, fileName))
+  }
+
+  def main(args: Array[String]): Unit = {
+    val path = args.head
+    val f = new File(path)
+    val works = listFilesRecursive(f, "work.toml")
+    println(works.map(_.getParent).mkString("\n"))
+    // println(listFilesRecursive(f, "work.toml").map(v => v.getPath).mkString("\n"))
+  
     val novel = Novel(
       "これが本文です。これが本文です。",
       NovelInfo("タイトルです", "/text", Some("aaaa"), Seq()),
       Original
     )
-    println(novel.generate())
   }
 }
--- src/main/scala/workbuilder/Novel.scala
+++ src/main/scala/workbuilder/Novel.scala
@@ -1,7 +1,9 @@
 package workbuilder
 
-import yamusca.imports._
 
+/*
+import yamusca.imports._
+*/
 case class NovelInfo(
     title: String,
     path: String,
@@ -13,11 +15,14 @@ case class Novel(
     text: String,
     info: NovelInfo,
     genre: Genre
-) extends Page {
+) {
+  /*
   override def generate(): String = {
     val context =
       Context("title" -> Value.of(info.title), "text" -> Value.of(text))
     val template = mustache.parse("<h1>{{title}}</h1><p>{{text}}</p>")
     mustache.render(template.toOption.get)(context)
   }
+  */
 }
+
