e0ae979b4cde6a486988b25db7a2ad07e5d8e323
sworltieary <shnovels@gmail.com>
2022-09-20 21:30:47 +0900

    use twirl template (wip)

--- src/main/scala/workbuilder/pages/AllTagPage.scala
+++ src/main/scala/workbuilder/pages/AllTagPage.scala
@@ -6,6 +6,7 @@ import java.nio.file.Paths
 import workbuilder.PageGenerator
 import workbuilder.Database
 import workbuilder.Template
+import workbuilder.html
 
 sealed trait AllTagPage
 
@@ -14,18 +15,7 @@ object AllTagPageObject extends AllTagPage
 object AllTagPage {
   implicit object AllTagPageGenerator extends PageGenerator[AllTagPage] {
     def generate(source: AllTagPage, database: Database): Map[Path, String] = {
-      val path = Paths.get("tags/index.html")
-      val novels = database.getNovels
-      val html = Template.htmlPage(
-        "タグ一覧",
-        s"""
-        |<h1>Tags</h1>
-        |${database.getTags
-            .sortBy(_.name)
-            .map(t => t.htmlTag(Some(novels.filter(_.hasTag(t)).length)))
-            .mkString}""".stripMargin
-      )
-      Map(path -> html)
+      Map(Paths.get("tags/index.html") -> html.alltag(database.getTags, database.getNovels).toString)
     }
   }
 }
