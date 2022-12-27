dc3731a57ee6086fc4d501eaece9d6f19036295f
sworliteary <shnovels@gmail.com>
2022-12-27 13:21:08 +0900

    色々修正

--- src/main/scala/workbuilder/Series.scala
+++ src/main/scala/workbuilder/Series.scala
@@ -37,7 +37,6 @@ object Series {
   implicit object SeriesGenerator extends PageGenerator[Series] {
     def generate(source: Series, database: Database): Map[Path, String] = {
       val novels = database.getNovels.filter(_.series.exists(_ == source)).sortBy(_.genre.directory.getName())
-      println(novels)
       Map(
         Paths.get(source.path).resolve("index.html") -> html
           .novels(
